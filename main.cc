795df248fca3f4d2d56b433a2be2a92f7dedcf92
swissChili <sasha@fastmail.com>
2018-11-24 14:19:22 -0800

    Simplified main

--- main.cc
+++ main.cc
@@ -10,32 +10,15 @@ namespace asio = boost::asio;
 #include <discordpp/websocket-websocketpp.hh>
 #include <discordpp/framework.hh>
 
-//#include <lib/nlohmannjson/src/json.hpp>
-//#include <nlohmann/json.hpp>
-
-std::string readTokenFile(std::string tokenFilePath);
 
 using json = nlohmann::json;
 using aios_ptr = std::shared_ptr<asio::io_service>;
 
 int main() {
     std::cout << "Starting bot...\n\n";
-
-    /*/
-     * Read token from token file.
-     * Tokens are required to communicate with Discord, and hardcoding tokens is a bad idea.
-     * If your bot is open source, make sure it's ignore by git in your .gitignore file.
-    /*/
-    std::string token;
-    if(boost::filesystem::exists("token.dat")){
-        token = readTokenFile("token.dat");
-    } else {
-        std::cerr << "CRITICAL: There is no valid way for Discord++ to obtain a token! Copy the example login.dat or token.dat to make one.\n";
-        exit(1);
-    }
+    std::string token = discordpp::tokenFromFile("token.dat");
 
     aios_ptr aios = std::make_shared<asio::io_service>();
-
     discordpp::Bot bot(
             aios,
             token,
@@ -47,36 +30,13 @@ int main() {
 
     framework.prompt = "--";
     // will reply to --hi with "Hello!"
-    framework.setCommand("hi", [&](std::string hi, json msg){
+    framework.setCommand("hi", [&](std::string, json msg){
         framework.messageReply(msg, "Hello!");
     });
 
-    bot.addHandler("PRESENCE_UPDATE", [](discordpp::Bot*, json) {
-        // ignore
-    });
-    bot.addHandler("TYPING_START", [](discordpp::Bot*, json) {
-        // ignore
+    framework.setCommand("hola", [&](std::string, json msg){
+        framework.messageReply(msg, "Hola!");
     });
 
     aios->run();
-
-    return 0;
-}
-
-std::string readTokenFile(std::string tokenFilePath){
-    std::ifstream tokenFile;
-    tokenFile.open(tokenFilePath);
-
-    std::string token;
-
-    if (tokenFile.is_open()) {
-        std::getline(tokenFile, token);
-    } else {
-        std::cerr << "CRITICAL: There is no such file as " + tokenFilePath + "! Copy the example login.dat to make one.\n";
-        exit(1);
-    }
-    tokenFile.close();
-    std::cout << "Retrieved token.\n\n";
-
-    return token;
 }
